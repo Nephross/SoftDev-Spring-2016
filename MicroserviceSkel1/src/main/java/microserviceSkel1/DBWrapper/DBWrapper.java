52386036a7835ff0a9cc9592124d0a7543874ba4
Peter <pete201c@stud.kea.dk>
2016-04-26 09:26:58 +0200

    started to create the example on db connection in microserviceskel1

--- MicroserviceSkel1/src/main/java/microserviceSkel1/DBWrapper/DBWrapper.java
+++ MicroserviceSkel1/src/main/java/microserviceSkel1/DBWrapper/DBWrapper.java
@@ -5,40 +5,10 @@
  */
 package microserviceSkel1.DBWrapper;
 
-
-import ResourcesPools.Super_DBWrapper;
-import java.util.concurrent.Future;
-
-
 /**
  *
  * @author Nephross
  */
-public class DBWrapper extends Super_DBWrapper {
-    	
-    public DBWrapper () {
-    	super();
-    }
+public class DBWrapper {
     
-    //Example method for using the connection and threadpool
-    public int test_Connection(int inputInt) {
-            if(inputInt < 0) {
-                    throw new IllegalArgumentException();
-            }
-
-            int outputInt = -1;
-            //This following line is the one where we send in the callable object we created into the threadpool executor to cue and
-            //then get the return value in the form of a Future<T>. 
-            Future<Integer> testFuture = executor.submit(new TestConCallable(inputInt, getConnection()));
-
-            try {
-                //recovering the contained int from the future.
-                    outputInt = testFuture.get(); 
-            }
-            catch(Exception e){
-                    e.printStackTrace();
-            }
-            //And returning the value.
-            return outputInt;
-	}	
 }
