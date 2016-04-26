582d0d02e042866d4ec3c919be7939dc44b512b9
Peter <pete201c@stud.kea.dk>
2016-04-26 09:32:08 +0200

    small change to configuration.. still untested.

--- SharedResources/src/main/java/ResourcesPools/HibernateSessionManager.java
+++ SharedResources/src/main/java/ResourcesPools/HibernateSessionManager.java
@@ -21,7 +21,7 @@ public class HibernateSessionManager {
     private static SessionFactory buildSessionFactory() {
         try {
         // Create the SessionFactory from hibernate.cfg.xml
-            return new AnnotationConfiguration().configure().addAnnotatedClass(User.class).buildSessionFactory();
+            return new AnnotationConfiguration().configure("/main/resources/hibernate.cfg.xml").addAnnotatedClass(User.class).buildSessionFactory();
         } catch (Throwable ex) {
         // Make sure you log the exception, as it might be swallowed
             System.err.println("SessionFactory creation failed." + ex);
