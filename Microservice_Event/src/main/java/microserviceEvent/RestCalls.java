c3b98bc7d8c36cc6c5352a8ef90a1edd948ccb70
Ronni Christophersen <Znake4@gmail.com>
2016-04-27 14:25:53 +0200

    added create event

--- Microservice_Event/src/main/java/microserviceEvent/RestCalls.java
+++ Microservice_Event/src/main/java/microserviceEvent/RestCalls.java
@@ -6,13 +6,20 @@
 package microserviceEvent;
 
 import Domain.Event;
+import Domain.Message;
+import Domain.Sub_Category;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import microserviceEvent.Repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,6 +40,16 @@ public class RestCalls {
     @PostConstruct
     public void init(){
         
+    }
+    
+    
+    @RequestMapping(value = "/create_event", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Event> createEvent(@RequestBody Event event){
+
+        eventRepository.save(event);
+        
+        ResponseEntity<Event> selectEvent = new ResponseEntity<>(event, HttpStatus.OK);
+        return selectEvent;
     }
     
     // call with http://localhost:8092/select_event?eventID=1
